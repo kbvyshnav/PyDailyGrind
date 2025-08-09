19f759c60e6a8c0862bbbad2255581403ea8b8ad
Vyshnav K B <vyshnavkb648@gmail.com>
2025-08-09 23:37:57 +0530

    added 2 type mehod to find prime  numbers.

--- primenumbers.py
+++ primenumbers.py
@@ -1,6 +1,37 @@
-#program to find prime numbers from 1 to 100
+'''-----program to find prime numbers from 1 to 100-------'''
 
+#Using method of Check divisors only up to the square root:
+for num in range(2, 101):
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
 
+#Using two pointer method :
+left = 2
+right = 100
+
+while left <= right:
+    # Check if left is prime
+    for i in range(2, int(left ** 0.5) + 1):
+        if left % i == 0:
+            break
+    else:
+        print(left)
+
+    # If left == right, avoid duplicate check (when pointers meet in the middle)
+    if left != right:
+        for i in range(2, int(right ** 0.5) + 1):
+            if right % i == 0:
+                break
+        else:
+            print(right)
+    left += 1
+    right -= 1
+        
 
 
 
