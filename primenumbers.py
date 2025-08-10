bec3417263817a87c98ec4dab135beb49abd5d54
Vyshnav K B <vyshnavkb648@gmail.com>
2025-08-10 23:59:50 +0530

    fa

--- primenumbers.py
+++ primenumbers.py
@@ -47,3 +47,11 @@ if count ==2:
     print(f"{num} is a prime number")
 else:
     print(f"{num} is not a prime number")    
+
+
+    
+if count ==2:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")    
+
